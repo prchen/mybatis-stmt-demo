382081e84f798fb5286484a60cbf7df0c5402abd
陈沛然 <chenpeiran@zhihu.com>
2021-04-15 10:03:26 +0800

    IDE init

--- src/main/java/com/github/prchen/mybatis_stmt_demo/App.java
+++ src/main/java/com/github/prchen/mybatis_stmt_demo/App.java
@@ -2,12 +2,9 @@ package com.github.prchen.mybatis_stmt_demo;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
